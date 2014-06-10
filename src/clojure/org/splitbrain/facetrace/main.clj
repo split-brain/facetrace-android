28f5e8f1c518c3d6c9adaabb175eeb940bd62c33
Dmitriy Morozov <dmmo@ciklum.com>
2014-06-11 00:05:40 +0300

    Android skeleton project.

---
+++ src/clojure/org/splitbrain/facetrace/main.clj
@@ -0,0 +1,13 @@
+(ns org.splitbrain.facetrace.main
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]))
+
+(defactivity org.splitbrain.facetrace.FacetraceActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "Hello from Clojure!"}]])))))
